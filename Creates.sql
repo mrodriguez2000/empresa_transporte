30f23991c0a316d8c57a68d9f267d2545d4dd2e4
unknown <migueledo17@mail.com>
2024-05-18 13:15:23 -0500

    Proyecto de la base de datos de una empresa de transporte

---
+++ Creates.sql
@@ -0,0 +1,217 @@
+-- Active: 1716011690015@@localhost@3306@empresa_transporte
+-- Creación de la base de datos
+CREATE DATABASE empresa_transporte;
+USE empresa_transporte;
+-- Creación de tabla viajeros o pasajeros
+CREATE TABLE
+    viajeros (
+        Viajero_ID INT AUTO_INCREMENT PRIMARY KEY,
+        Nombre VARCHAR(30) NOT NULL,
+        Genero VARCHAR(30),
+        Fecha_Nacimiento_Viajero DATE,
+        Edad_Viajero INT,
+        Cantidad_Viajes INT DEFAULT 0
+    );
+
+-- Creación de tabla de departamentos (departamentos de Colombia)
+CREATE TABLE
+    departamentos (
+        Departamento_ID INT AUTO_INCREMENT PRIMARY KEY,
+        Nombre_Departamento VARCHAR(30) NOT NULL
+    );
+
+-- Creación de tabla de ciudades
+CREATE TABLE
+    ciudades (
+        Ciudad_ID INT AUTO_INCREMENT PRIMARY KEY,
+        Ciudad_Nombre VARCHAR(30),
+        Departamento_ID INT NOT NULL,
+        CONSTRAINT FK_DEPARTAMENTO FOREIGN KEY (Departamento_ID) REFERENCES departamentos (Departamento_ID) ON DELETE CASCADE ON UPDATE CASCADE
+    );
+
+-- Se crean tablas de ciudad origen y ciudad destino, las cuales tienen como función alimentar la tabla de ruta, ya que en una empresa de viajes siempre hay una ciudad desde la que parte un bus hasta una ciudad de llegada o destino
+CREATE TABLE ciudad_origen (
+    Ciudad_Origen_ID INT PRIMARY KEY,
+    Ciudad_Origen_Nombre VARCHAR(30),
+    CONSTRAINT FK_CIUDAD_ORIGEN FOREIGN KEY(Ciudad_Origen_ID) REFERENCES ciudades(Ciudad_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE ciudad_destino (
+    Ciudad_Destino_ID INT PRIMARY KEY,
+    Ciudad_Destino_Nombre VARCHAR(30),
+    CONSTRAINT FK_CIUDAD_DESTINO FOREIGN KEY(Ciudad_Destino_ID) REFERENCES ciudades(Ciudad_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+-- Creación de tabla de rutas
+CREATE TABLE rutas (
+    Ruta_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Ciudad_Origen_ID INT,
+    Ciudad_Destino_ID INT,
+    CONSTRAINT FK_CIUDAD_ORIGEN_RUTA FOREIGN KEY(Ciudad_Origen_ID) REFERENCES ciudad_origen(Ciudad_Origen_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT FK_CIUDAD_DESTINO_RUTA FOREIGN KEY(Ciudad_Destino_ID) REFERENCES ciudad_destino(Ciudad_Destino_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+-- Creación de la tabla de conductores
+CREATE TABLE
+    conductores (
+        Conductor_ID INT AUTO_INCREMENT PRIMARY KEY,
+        Nombre_Conductor VARCHAR(30) NOT NULL,
+        Genero_Conductor VARCHAR(30),
+        Fecha_Nacimiento_Conductor DATE,
+        Edad_Conductor INT,
+        Fecha_Contratacion DATE NOT NULL,
+        Total_Viajes_Asignados INT DEFAULT 0,
+        Cantidad_Viajes_Realizados INT DEFAULT 0,
+        Cantidad_Viajes_Pendientes INT AS (Total_Viajes_Asignados - Cantidad_Viajes_Realizados)
+    );
+-- Creación de la tabla de marcas. Contiene un listado de las marcas de los buses
+CREATE TABLE
+    marcas (
+        Marca_ID INT AUTO_INCREMENT PRIMARY KEY,
+        Marca_Nombre VARCHAR(30) NOT NULL
+    );
+
+-- Creación de la tabla de buses
+CREATE TABLE
+    buses (
+        Bus_ID INT AUTO_INCREMENT PRIMARY KEY,
+        Capacidad_Pasajeros INT,
+        Marca_ID INT NOT NULL,
+        Municipio_Matricula_ID INT,
+        Cantidad_Viajes_Asignados INT DEFAULT 0,
+        Cantidad_Viajes_Finalizados INT DEFAULT 0,
+        Cantidad_Viajes_Pendientes INT AS (Cantidad_Viajes_Asignados - Cantidad_Viajes_Finalizados),
+        CONSTRAINT FK_MUNICIPIO_MATRICULA FOREIGN KEY (Municipio_Matricula_ID) REFERENCES ciudades (Ciudad_ID) ON DELETE CASCADE ON UPDATE CASCADE,
+        CONSTRAINT FK_MARCA_BUS FOREIGN KEY (Marca_ID) REFERENCES marcas (Marca_ID) ON DELETE CASCADE ON UPDATE CASCADE
+    );
+
+-- Tabla que contiene todos los viajes programados
+CREATE TABLE viajes (
+    Viaje_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Bus_ID INT NOT NULL,
+    Fecha_Programada_Viaje DATETIME NOT NULL,
+    Estado_Viaje VARCHAR(30) DEFAULT 'Programado',
+    CONSTRAINT FK_BUSES_VIAJES FOREIGN KEY(Bus_ID) REFERENCES buses(Bus_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+-- Llegadas de los viajes
+CREATE TABLE llegadas (
+    Llegada_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Viaje_ID INT NOT NULL,
+    Fecha_Llegada DATETIME NOT NULL,
+    CONSTRAINT FK_VIAJES_LLEGADAS FOREIGN KEY (Viaje_ID) REFERENCES viajes(Viaje_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+-- En caso de que un viaje se reprograme bajo cualquier circunstancia se crea una tabla llamada viajes reprogramados
+CREATE TABLE Viajes_Reprogramados (
+    Reprogramacion_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Viaje_ID INT,
+    Fecha_Programada_Viaje_Inicial DATETIME, -- Primera fecha en la que se programó el viaje
+    Fecha_Programada_Viaje_Nueva DATETIME, -- Nueva fecha en la que se programa el viaje
+    Estado_Viaje VARCHAR(30) DEFAULT 'Reprogramado',
+    CONSTRAINT FK_VIAJE_REPROGRAMADO FOREIGN KEY (Viaje_ID) REFERENCES viajes(Viaje_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+-- Se va a crear una tabla que mantenga relaciones entre las tablas viajes y conductores, debido a que un viaje puede tener asignados varios conductores y varios conductores pueden estar asignados para un viaje
+CREATE TABLE Viaje_Conductor (
+    Registro_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Viaje_ID INT,
+    Conductor_ID INT,
+    Estado_Viaje VARCHAR(30) DEFAULT 'Programado',
+    CONSTRAINT FK_VIAJE_CONDUCTOR FOREIGN KEY (Viaje_ID) REFERENCES viajes(Viaje_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT FK_CONDUCTOR_VIAJE FOREIGN KEY (Conductor_ID) REFERENCES conductores(Conductor_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+-- Listado de terminales o instalaciones donde parte el bus
+CREATE TABLE
+    terminales (
+        Terminal_ID INT AUTO_INCREMENT PRIMARY KEY,
+        Nombre_Terminal VARCHAR(100) NOT NULL,
+        Ciudad_Ubicacion_ID INT,
+        CONSTRAINT FK_CIUDAD_UBICACION FOREIGN KEY (Ciudad_Ubicacion_ID) REFERENCES ciudades (Ciudad_ID) ON DELETE CASCADE ON UPDATE CASCADE
+    );
+
+-- Listado de trabajadores que se encargan de elaborar el tiquete a los pasajeros
+CREATE TABLE
+    taquillero (
+        Taquillero_ID INT AUTO_INCREMENT PRIMARY KEY,
+        Nombre_Taquillero VARCHAR(30) NOT NULL,
+        Genero_Taquillero VARCHAR(30),
+        Fecha_Nacimiento_Taquillero DATE,
+        Edad_Taquillero INT,
+        Fecha_Contratacion DATE NOT NULL,
+        Cantidad_Tiquetes INT DEFAULT 0,
+        Terminal_ID INT,
+        CONSTRAINT FK_TERMINAL_OPERACION FOREIGN KEY (Terminal_ID) REFERENCES terminales(Terminal_ID)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+    );
+
+CREATE TABLE Metodo_Pago (
+    Metodo_Pago_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Metodo_Pago_Nombre VARCHAR(30)
+);
+
+-- Tabla que contiene información detallada del tiquete, como la información del viajero, el taquillero o trabajador que hizo el tiquete, el bus asignado para el viaje, la ruta, la fecha en la que se compra el tiquete y una fecha en la que está programado el viaje
+CREATE TABLE tiquete (
+    Compra_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Viajero_ID INT,
+    Taquillero_ID INT,
+    Viaje_ID INT,
+    Ruta_ID INT,
+    Metodo_Pago_ID INT,
+    Valor_Tiquete INT,
+    Aplica_Descuento BOOLEAN,
+    Valor_Descuento FLOAT,
+    Valor_Pagado INT AS (
+        CASE 
+            WHEN Aplica_Descuento = TRUE THEN (Valor_Tiquete - (Valor_Tiquete * Valor_Descuento))  
+            ELSE Valor_Tiquete 
+        END
+    ),
+    Fecha_Compra DATETIME,
+    CONSTRAINT FK_VIAJERO_TIQUETE FOREIGN KEY (Viajero_ID) REFERENCES viajeros(Viajero_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT FK_VIAJE_TIQUETE FOREIGN KEY (Viaje_ID) REFERENCES viajes(Viaje_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT FK_TAQUILLERO_TIQUETE FOREIGN KEY (Taquillero_ID) REFERENCES taquillero(Taquillero_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT FK_RUTA_VIAJE_TIQUETE FOREIGN KEY (Ruta_ID) REFERENCES rutas(Ruta_Id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT FK_METODO_DE_PAGO FOREIGN KEY (Metodo_Pago_ID) REFERENCES Metodo_Pago(Metodo_Pago_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+-- Se realiza una tabla de empleados que renuncian a la empresa
+CREATE TABLE
+    trabajadores_desvinculados (
+        Trabajador_ID INT PRIMARY KEY,
+        Nombre_Trabajador VARCHAR(30),
+        Genero_Trabajador VARCHAR(30),
+        Fecha_Nacimiento_Trabajador VARCHAR(30),
+        Fecha_Contrato_Trabajador DATE,
+        Fecha_Retiro DATE
+    );
---
+++ Triggers.sql
@@ -0,0 +1,197 @@
+-- Active: 1716011690015@@localhost@3306@empresa_transporte
+-- Triggers que calcula las edades
+CREATE TRIGGER calcula_edad_conductor 
+BEFORE INSERT ON conductores 
+FOR EACH ROW
+BEGIN
+    SET NEW.Edad_Conductor = TIMESTAMPDIFF(YEAR, NEW.Fecha_Nacimiento_Conductor, CURDATE());
+END;
+
+CREATE TRIGGER calcula_edad_viajero BEFORE INSERT ON viajeros FOR EACH ROW
+BEGIN
+	SET NEW.Edad_Viajero = TIMESTAMPDIFF(YEAR, NEW.Fecha_Nacimiento_Viajero, CURDATE());
+END;
+
+CREATE TRIGGER calcula_edad_taquillero BEFORE INSERT ON taquillero FOR EACH ROW
+BEGIN
+	SET NEW.Edad_Taquillero = TIMESTAMPDIFF(YEAR, NEW.Fecha_Nacimiento_Taquillero, CURDATE());
+END;
+
+-- Trigger que evita insertar trabajadores menores de edad
+CREATE TRIGGER conductores_menores_edad BEFORE INSERT 
+ON conductores FOR EACH ROW 
+BEGIN 
+DECLARE
+	Edad INT;
+    SET Edad = NEW.Edad_Conductor;
+	IF Edad < 18 THEN SIGNAL SQLSTATE '45000'
+	SET
+	    MESSAGE_TEXT = 'El usuario debe ser mayor de 18 años.';
+END
+	IF;
+END;
+
+CREATE TRIGGER taquilleros_menores_edad BEFORE INSERT 
+ON taquillero FOR EACH ROW 
+BEGIN 
+DECLARE
+	Edad INT;
+	SET Edad = NEW.Edad_Taquillero;
+	IF Edad < 18 THEN SIGNAL SQLSTATE '45000'
+	SET
+	    MESSAGE_TEXT = 'El usuario debe ser mayor de 18 años.';
+END
+	IF;
+END;
+
+-- Trigger que inserta automaticamente registros en la tabla de conductores desvinculados antes de hacer la eliminación
+CREATE TRIGGER conductores_desvinculados BEFORE DELETE 
+ON conductores FOR EACH ROW 
+BEGIN 
+	INSERT INTO
+	    trabajadores_desvinculados (
+	        `Trabajador_ID`, `Nombre_Trabajador`, `Genero_Trabajador`, `Fecha_Nacimiento_Trabajador`, `Fecha_Contrato_Trabajador`, `Fecha_Retiro`
+	    )
+	VALUES (
+	        OLD.Conductor_ID, OLD.Nombre_Conductor, OLD.Genero_Conductor, OLD.Fecha_Nacimiento_Conductor, OLD.Fecha_Contratacion, CURDATE()
+	    );
+END;
+
+CREATE TRIGGER taquilleros_desvinculados BEFORE DELETE 
+ON taquillero FOR EACH ROW 
+BEGIN 
+	INSERT INTO
+	    trabajadores_desvinculados (
+	        `Trabajador_ID`, `Nombre_Trabajador`, `Genero_Trabajador`, `Fecha_Nacimiento_Trabajador`, `Fecha_Contrato_Trabajador`, `Fecha_Retiro`
+	    )
+	VALUES (
+	        OLD.Taquillero_ID, OLD.Nombre_Taquillero, OLD.Genero_Taquillero, OLD.Fecha_Nacimiento_Taquillero, OLD.Fecha_Contratacion, CURDATE()
+	    );
+END;
+
+-- Trigger que evita insertar registros en la tabla tiquete en caso de algún error, por ejemplo, si al ingresar un registro que no aplica descuento
+CREATE TRIGGER valor_descuento BEFORE INSERT ON tiquete 
+FOR EACH ROW 
+BEGIN 
+DECLARE
+	Porcentaje_Descuento FLOAT;
+DECLARE
+	Aplica_Descuento BOOLEAN;
+	SET Porcentaje_Descuento = NEW.Valor_Descuento;
+	SET Aplica_Descuento = NEW.Aplica_Descuento;
+	IF Aplica_Descuento = FALSE THEN SIGNAL SQLSTATE '45000'
+	SET
+	    MESSAGE_TEXT = 'No hay descuento';
+END
+	IF;
+END;
+
+-- Trigger que cambia el estado de viaje a finalizado cuando se inserta un viaje en la tabla de llegadas, ya que por defecto en la tabla de viajes se encuentra en estado programado
+CREATE TRIGGER viajes_finalizados AFTER INSERT ON llegadas 
+FOR EACH ROW 
+BEGIN 
+	UPDATE viajes
+	SET
+	    Estado_Viaje = 'Finalizado'
+	WHERE
+	    viajes.`Viaje_ID` = NEW.`Viaje_ID`;
+END;
+
+-- Creación de un trigger que inserta registros en una tabla (viajes_reprogramados) cuando se cambia la fecha del viaje. Esto ocurrirá después de un UPDATE en la tabla viajes
+CREATE TRIGGER viajes_reprogramados AFTER UPDATE ON 
+viajes FOR EACH ROW 
+BEGIN 
+	INSERT INTO
+	    viajes_reprogramados (
+	        `Viaje_ID`, `Fecha_Programada_Viaje_Inicial`, `Fecha_Programada_Viaje_Nueva`
+	    )
+	SELECT NEW.`Viaje_ID`, OLD.`Fecha_Programada_Viaje`, NEW.`Fecha_Programada_Viaje`
+	FROM viajes
+	WHERE
+	    viajes.`Viaje_ID` = NEW.`Viaje_ID`
+	    AND `Estado_Viaje` = 'Programado';
+END;
+
+-- Se realizan pruebas con el trigger y funcionó de forma correcta
+
+CREATE TRIGGER no_actualizar_viajes_finalizados BEFORE 
+UPDATE ON viajes FOR EACH ROW 
+BEGIN 
+DECLARE
+	Viaje_Estado VARCHAR(30);
+	SET Viaje_Estado = OLD.Estado_Viaje;
+	IF Viaje_Estado = 'Finalizado' THEN SIGNAL SQLSTATE '45000'
+	SET
+	    MESSAGE_TEXT = 'No es posible actualizar viajes finalizados, por favor verifica la información que deseas actualizar';
+END
+	IF;
+END;
+
+-- El siguiente trigger consiste en actualizar el estado del viaje en la tabla viaje_conductor, por defecto se encontrará en estado programado, y una vez finalizado el viaje se va a cambiar a estado finalizado
+CREATE TRIGGER viaje_estado AFTER UPDATE ON viajes 
+FOR EACH ROW 
+BEGIN 
+	UPDATE viaje_conductor
+	SET
+	    `Estado_Viaje` = NEW.`Estado_Viaje`
+	WHERE
+	    `Viaje_ID` = NEW.`Viaje_ID`;
+END;
+
+-- Trigger que cuenta la cantidad de tiquetes que ha comprado un usuario
+CREATE TRIGGER cantidad_viajes AFTER INSERT ON tiquete FOR EACH ROW
+BEGIN
+    UPDATE viajeros SET `Cantidad_Viajes` = (SELECT COUNT(NEW.Viajero_ID) FROM tiquete t
+    WHERE `Viajero_ID` = NEW.`Viajero_ID`)
+    WHERE `Viajero_ID` = NEW.`Viajero_ID`;
+END;
+
+-- El trigger cuenta la cantidad de tiquetes o las veces que un taquillero atendió
+CREATE TRIGGER cantidad_tiquetes AFTER INSERT ON tiquete FOR EACH ROW
+BEGIN
+	UPDATE taquillero SET `Cantidad_Tiquetes` = (SELECT COUNT(`Taquillero_ID`) FROM tiquete WHERE `Taquillero_ID` = NEW.`Taquillero_ID`)
+	WHERE `Taquillero_ID` = NEW.`Taquillero_ID`;
+END;
+
+-- Trigger que cambia el estado de un viaje reprogramado a finalizado en caso de que el viaje registrado bajo el Viaje_ID tenga registro de una llegada
+CREATE TRIGGER viajes_reprogramados_actualizados AFTER UPDATE ON viajes FOR EACH ROW
+BEGIN
+	UPDATE viajes_reprogramados SET `Estado_Viaje` = 'Finalizado' WHERE `Viaje_ID` = NEW.`Viaje_ID`
+	AND NEW.`Estado_Viaje` = 'Finalizado';
+END;
+
+-- Trigger que se encargue de sumar la cantidad de viajes asignados que tiene un bus. El proceso se ejecuta después de insertar un registro en la tabla de viajes
+CREATE TRIGGER viajes_asignados_buses AFTER INSERT ON viajes FOR EACH ROW
+BEGIN
+	UPDATE buses SET `Cantidad_Viajes_Asignados` = (SELECT COUNT(`Viaje_ID`) FROM viajes WHERE `Bus_ID` = NEW.`Bus_ID`)
+	WHERE `Bus_ID` = NEW.`Bus_ID`;
+END;
+
+-- Trigger que se encargue de sumar la cantidad de viajes finalizados que tiene un bus. El proceso se ejecuta después de insertar un registro en la tabla de viajes, además, después de actualizada la información hará una resta entre la cantidad de viajes asignados y finalizados, con la finalidad de determinar la cantidad real de viajes que tiene pendientes o asignados
+CREATE TRIGGER viajes_finalizados_buses AFTER UPDATE ON viajes FOR EACH ROW
+BEGIN
+	UPDATE buses SET `Cantidad_Viajes_Finalizados` = (SELECT COUNT(`Viaje_ID`) FROM viajes WHERE `Bus_ID` = NEW.`Bus_ID` AND `Estado_Viaje` = 'Finalizado') WHERE `Bus_ID` = NEW.`Bus_ID`;
+END;
+
+-- Trigger que cuenta la cantidad de viajes finalizados que tuvo asignado un conductor. El proceso consiste en actualizar la cantidad de viajes que realizó un conductor después de que se actualicé la tabla de viaje_conductor
+CREATE TRIGGER cantidad_viajes_realizados AFTER UPDATE ON viaje_conductor FOR EACH ROW
+BEGIN
+	UPDATE conductores SET `Cantidad_Viajes_Realizados` = (SELECT COUNT(`Conductor_ID`) FROM viaje_conductor WHERE `Estado_Viaje` = 'Finalizado' AND `Conductor_ID` = NEW.`Conductor_ID`)
+	WHERE `Conductor_ID` = NEW.`Conductor_ID`;
+END;
+
+CREATE TRIGGER total_viajes_conductor AFTER INSERT ON viaje_conductor FOR EACH ROW
+BEGIN
+	UPDATE conductores SET `Total_Viajes_Asignados` = (SELECT COUNT(`Conductor_ID`) FROM viaje_conductor WHERE `Conductor_ID` = NEW.`Conductor_ID`)
+	WHERE `Conductor_ID` = NEW.`Conductor_ID`;
+END;
+
+-- El presente trigger evitará que una fecha de compra de un determinado tiquete sea mayor que la fecha del viaje programado
+CREATE TRIGGER fecha_compra_tiquete BEFORE INSERT ON tiquete FOR EACH ROW
+BEGIN
+	DECLARE fecha_programada DATETIME;
+	SELECT v.`Fecha_Programada_Viaje` INTO fecha_programada FROM viajes v WHERE NEW.`Viaje_ID` = v.`Viaje_ID`;
+	IF NEW.Fecha_Compra > fecha_programada THEN SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error de información, la fecha de compra es mayor que la fecha de viaje programada.';
+    END IF;	
+END;
